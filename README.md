88a349aeed2cd45445e785df5c65c67454b66742
Robert <robertleelivingston@gmail.com>
2017-12-30 17:06:36 -0600

    Update handlers and README

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Lunch Picker API for HipChat
 
 This is a command extension to be used with HipChat that picks a lunch based on existing places.
-  - Add a place to eat using /lunch add <name>
-  - Have a random place picked for lunch using /lunch
+  - Add a place to eat using `/lunch add {name}`
+  - Have a random place picked for lunch using `/lunch`
+  - See how many places are being chosen from with `/lunch count`
 
 ### New Features!
 
   - The ability to add a place to eat!
+  - The ability to see how many places can be chosen from!
 
 Hosted in Azure using Azure CLI.
 Feel free to fork and make a pull request.
@@ -22,13 +24,25 @@ $ npm start
 ```
 
 Use PostMan to make a POST request.
+
+```
+{
+  "item" : {
+    "message" : {
+      "message": "/lunch {params}"
+    }
+  }
+}
+```
 - A POST request with no body will retrieve a random place
-- A POST request with "add" : "RestaurantName" will create a new restaurant for our engineers to possibly eat at
+- A POST requests must be formatted as a HipChat Request
 - A POST request with any other command will return an error
+
+### Parameters : 
+- add {Restaurant Name}
+- count
 
 ### Todos
  - Have a DELETE to get rid of places to eat
  - Have a GET to return all places to eat
  - Move mongodb dependency into a module to be used with more handlers
-
-
--- handlers/lunch.js
+++ handlers/lunch.js
@@ -1,35 +1,85 @@
 'use strict';
 
 const mongo = require('mongodb').MongoClient;
-const mongoUrl = "mongodb://defaultUser:bearbear@ds119718.mlab.com:19718/fcc-db";
+
+// Use your Mongo URL here
+var mongoUrl;
 var db;
 
 module.exports = {
     post: function lunch_post(req, res) {
         if(req.body.constructor === Object && Object.keys(req.body).length > 0)
         {
-            var placeName = req.body.add;
-            if(placeName === null || placeName === undefined)
+            var messageTxt = req.body.item.message.message;
+            var splitMessage = messageTxt.split(' ');
+            var command = splitMessage[1];
+            if(splitMessage.length === 1)
             {
-                var jsonResponse = {
-                    "color": "red",
-                    "message": "Unknown command (wat). Please use 'add' and <Restaurant Name>",
-                    "notify": false,
-                    "message_format": "text"
-                }
-                res.json(jsonResponse);
-            }
-            else {
                 mongo.connect(mongoUrl, (err, client) =>{
                     if (err) {
                         return console.log(err);
+                    }
+    
+                    db = client.db('fcc-db');
+                    db.collection('places').find().toArray(function(err, results){
+                        if(err) {
+                            client.close();
+                            return console.log(err);
+                        }
+                        var length = results.length;
+                        var placeToEat = results[Math.floor(Math.random() * length)];
+                        var jsonResponse = {
+                            "color": "green",
+                            "message": "The place to eat is " + placeToEat.name + "!",
+                            "notify": false,
+                            "message_format": "text"
+                        }
+                        res.json(jsonResponse);
+                    });     
+                });
+            }
+            else if(command === "count")
+            {
+                mongo.connect(mongoUrl, (err, client) =>{
+                    if (err) {
+                        return console.log(err);
+                    }
+    
+                    db = client.db('fcc-db');
+                    db.collection('places').find().toArray(function(err, results){
+                        if(err) {
+                            client.close();
+                            return console.log(err);
+                        }
+                        var length = results.length;
+                        var placeToEat = results[Math.floor(Math.random() * length)];
+                        var jsonResponse = {
+                            "color": "yellow",
+                            "message": "Choosing from " + length + " places! (fiestaparrot)",
+                            "notify": false,
+                            "message_format": "text"
+                        }
+                        res.json(jsonResponse);
+                    });     
+                });
+            }
+            else if(command === "add" && splitMessage.length !== 2)
+            {
+                mongo.connect(mongoUrl, (err, client) =>{
+                    if (err) {
+                        return console.log(err);
+                    }
+                    var restaurant = "";
+                    for(var i = 2; i < splitMessage.length; i++)
+                    {
+                        restaurant += splitMessage[i] + " ";
                     }
 
                     db = client.db('fcc-db');
                     
                     db.collection('places').insertOne(
                         {
-                            name: placeName
+                            name: restaurant
                         },
                         function (err, res) {
                             if (err) {
@@ -41,33 +91,22 @@ module.exports = {
     
                     var jsonResponse = {
                         "color": "green",
-                        "message": placeName + " was added successfully! (dealwithitparrot)",
+                        "message": restaurant + "was added successfully! (dealwithitparrot)",
                         "notify": false,
                         "message_format": "text"
                     }
                     res.json(jsonResponse);
                 });
             }
-        }
-        else { 
-            mongo.connect(mongoUrl, (err, client) =>{
-                if (err) {
-                    return console.log(err);
+            else {
+                var jsonResponse = {
+                    "color": "red",
+                    "message": "Unknown command (wat). Please use 'add' and <Restaurant Name>.",
+                    "notify": false,
+                    "message_format": "text"
                 }
-
-                db = client.db('fcc-db');
-                db.collection('places').find().toArray(function(err, results){
-                    var length = results.length;
-                    var placeToEat = results[Math.floor(Math.random() * length)];
-                    var jsonResponse = {
-                        "color": "green",
-                        "message": "The place to eat is " + placeToEat.name + "!",
-                        "notify": false,
-                        "message_format": "text"
-                    }
-                    res.json(jsonResponse);
-                });     
-            });
+                res.json(jsonResponse);
+            }
         }
     }
 };
